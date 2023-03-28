189e6162ccbfaa095e7bf25e5de270c6c146d116
Claire Tregunna <c.tregunna1980@gmail.com>
2023-03-28 18:39:19 +0100

    Added function to like a post

--- app.js
+++ app.js
@@ -1,68 +1,86 @@
 const postContainer = document.getElementById("post-container");
 
-//Render using for loop
-function oldagramPosts() {
-    for (let i = 0; i < posts.length; i ++) {
-        postContainer.innerHTML +=
-        `
-            <section id="post">
-                <div class="post-header">
-                    <img src="${posts[i].avatar}" alt="${posts[i].username} avatar" class="avatar post-avatar">
-                    <div class="post-name">
-                        <p class="post-author">${posts[i].name}</p>
-                        <p class="post-location">${posts[i].location}</p>
-                    </div>
-                </div>
-                <div class="post-content">
-                    <img src="${posts[i].post}" alt="Selfie of ${posts[i].name}">
-                </div>
-                <div class="post-interaction">
-                    <img src="images/icon-heart.png" alt="heart icon" class="icon">
-                    <img src="images/icon-comment.png" alt="comment icon" class="icon">
-                    <img src="images/icon-dm.png" alt="dm icon" class="icon">
-                </div>
-                <div class="post-likes">
-                    <p class="bold">${posts[i].likes} likes</p>
-                </div>
-                <div class="post-comments">
-                    <p><span class="bold">${posts[i].username}</span>${posts[i].comment}</p>
-                </div>
-            </section>
-        `
-    }
-}
-
-oldagramPosts();
-
 
 //render using foEach loop
-// posts.forEach(post => { //this creates a post for each part of the array
-//     createPost(post);
-// })
 
-// function createPost(posts) { //posts is the parameter to get the data into the html from the array
-//     postContainer.innerHTML += `
-//     <section id="post">
-//         <div class="post-header">
-//             <img src="${posts.avatar}" alt="${posts.username} avatar" class="avatar post-avatar">
-//             <div class="post-name">
-//                 <p class="bold">${posts.name}</p>
-//                 <p class="post-location">${posts.location}</p>
-//             </div>
-//         </div>
-//         <div class="post-content">
-//             <img src="${posts.post}" alt="Selfie of ${posts.name}">
-//         </div>
-//         <div class="post-interaction">
-//             <img src="images/icon-heart.png" alt="heart icon" class="icon heart">
-//             <img src="images/icon-comment.png" alt="comment icon" class="icon">
-//             <img src="images/icon-dm.png" alt="dm icon" class="icon">
-//         </div>
-//         <div class="post-likes">
-//             <p class="bold likes">${posts.likes} likes</p>
-//         </div>
-//         <div class="post-comments">
-//             <p><span class="bold">${posts.username}</span>${posts.comment}</p>
-//         </div>
-//     </section>`
-// }
+function getPostHtml() {
+    let postHtml = ``;
+
+
+
+    posts.forEach(post => {
+
+        let likeIconClass = '';
+        if (post.isLiked) {
+            likeIconClass = "fa-solid"
+        }
+
+        postHtml += `
+        <section id="post">
+            <div class="post-header">
+                <img src="${post.avatar}" alt="${post.username} avatar" class="avatar post-avatar">
+                <div class="post-name">
+                    <p class="bold">${post.name}</p>
+                    <p class="post-location">${post.location}</p>
+                </div>
+            </div>
+            <div class="post-content">
+                <img src="${post.post}" alt="Selfie of ${post.name}">
+            </div>
+            <div class="post-interaction">
+                <i class="fa-regular fa-heart ${likeIconClass}" id="heart" data-like="${post.username}"></i>
+                <img src="images/icon-comment.png" alt="comment icon" class="icon">
+                <img src="images/icon-dm.png" alt="dm icon" class="icon">
+            </div>
+            <div class="post-likes">
+                <p class="bold likes">${post.likes} likes</p>
+            </div>
+            <div class="post-comments">
+                <p><span class="bold">${post.username}</span>${post.comment}</p>
+            </div>
+        </section>`
+    })
+    return postHtml
+}
+
+function render(){
+    postContainer.innerHTML = getPostHtml()
+}
+
+render()
+
+
+// //Function to add likes
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like) {
+        likePost(e.target.dataset.like)
+    }
+})
+
+function likePost(postUserName) {
+    let icon = document.getElementById("heart")
+    console.log(postUserName)
+
+    const targetPostObj = posts.filter(function(post){
+        return post.username === postUserName;
+    })[0]
+
+    if(targetPostObj.isLiked) {
+        targetPostObj.likes--
+        icon.classList.add("fa-regular")
+        icon.classList.remove("fa-solid")
+    } else {
+        targetPostObj.likes++
+        icon.classList.remove("fa-regular")
+        icon.classList.add("fa-solid")
+    }
+    targetPostObj.isLiked = !targetPostObj.isLiked;
+    console.log(targetPostObj.likes)
+    console.log(targetPostObj.isLiked)
+    render()
+}
+
+
+
+
+// <img src="images/icon-heart.png" alt="heart icon" class="icon heart" data-like="${post.username}">
--- code.js
+++ code.js
@@ -77,3 +77,38 @@ heart.addEventListener('dblclick', function() {
     }
 
 })
+
+
+//Render using for loop
+// function oldagramPosts() {
+//     for (let i = 0; i < posts.length; i ++) {
+//         postContainer.innerHTML +=
+//         `
+//             <section id="post">
+//                 <div class="post-header">
+//                     <img src="${posts[i].avatar}" alt="${posts[i].username} avatar" class="avatar post-avatar">
+//                     <div class="post-name">
+//                         <p class="post-author">${posts[i].name}</p>
+//                         <p class="post-location">${posts[i].location}</p>
+//                     </div>
+//                 </div>
+//                 <div class="post-content">
+//                     <img src="${posts[i].post}" alt="Selfie of ${posts[i].name}">
+//                 </div>
+//                 <div class="post-interaction">
+//                     <img src="images/icon-heart.png" alt="heart icon" class="icon">
+//                     <img src="images/icon-comment.png" alt="comment icon" class="icon">
+//                     <img src="images/icon-dm.png" alt="dm icon" class="icon">
+//                 </div>
+//                 <div class="post-likes">
+//                     <p class="bold">${posts[i].likes} likes</p>
+//                 </div>
+//                 <div class="post-comments">
+//                     <p><span class="bold">${posts[i].username}</span>${posts[i].comment}</p>
+//                 </div>
+//             </section>
+//         `
+//     }
+// }
+
+// oldagramPosts();
